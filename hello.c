777708b9879ff21ff69e86ab4f217f44accf0262
Yuya Minami <yuya373@me.com>
2018-01-26 13:28:01 +0900

    hello.c

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* hello.c -> gcc package(c preprosessor -> c compiler) ->
+   binutils(binary utils) package(assembler -> linker) -> a.out(assembler output) */
+int main() {
+  printf("Hello, world!\n");
+  return 123;
+}
